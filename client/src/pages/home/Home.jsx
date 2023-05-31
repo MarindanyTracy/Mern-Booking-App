bc92b2775a471e71434e1b37cec60e6ea6fd5812
MarindanyTracy <marindanytracy0@gmail.com>
2023-05-31 19:19:51 +0300

    Adding Property List component

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -2,6 +2,7 @@ import './Home.css';
 import Navbar from '../../components/Navbar/Navbar';
 import Header from '../../components/Header/Header';
 import Featured from '../../components/Featured/Featured';
+import PropertyList from '../../components/PropertyList/PropertyList';
 
 const Home = () => {
   return (
@@ -10,6 +11,10 @@ const Home = () => {
       <Header />
       <div className="homeContainer">
         <Featured />
+        <h1 className="homeTitle">
+          Browse by property name
+        </h1>
+        <PropertyList />
       </div>
     </div>
   )
