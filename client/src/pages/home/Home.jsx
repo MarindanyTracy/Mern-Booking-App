3d10d854956a9d8920d42ed01cc02117e906fce1
MarindanyTracy <marindanytracy0@gmail.com>
2023-05-31 19:34:58 +0300

    Featured Property component added

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -3,6 +3,7 @@ import Navbar from '../../components/Navbar/Navbar';
 import Header from '../../components/Header/Header';
 import Featured from '../../components/Featured/Featured';
 import PropertyList from '../../components/PropertyList/PropertyList';
+import FeaturedProperties from '../../components/featuredProperties/FeaturedProperties';
 
 const Home = () => {
   return (
@@ -15,6 +16,8 @@ const Home = () => {
           Browse by property name
         </h1>
         <PropertyList />
+        <h1 className="homeTitle">Homes guests love</h1>
+        <FeaturedProperties />
       </div>
     </div>
   )
