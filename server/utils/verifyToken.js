731e6973aa217ce95eec073f2b56d7ae1e831cd4
MarindanyTracy <marindanytracy0@gmail.com>
2023-06-05 11:22:21 +0300

    Room Model

--- server/utils/verifyToken.js
+++ server/utils/verifyToken.js
@@ -14,20 +14,20 @@ export const verifyToken = (req,res,next) => {
 }
 
 export const verifyUser = (req,res,next) => {
-  verifyToken(req,res, () => {
+  verifyToken(req,res,next, () => {
     if(req.user.id === req.params.id || req.user.isAdmin) {
       next()
     } else {
-      if(err) return next(createError(403, "You are not authorized!"));
+       return next(createError(403, "You are not authorized!"));
     }
   })
 }
 export const verifyAdmin = (req,res,next) => {
-  verifyToken(req,res, () => {
-    if(req.user.id === req.user.isAdmin) {
+  verifyToken(req,res,next, () => {
+    if(req.user.isAdmin) {
       next()
     } else {
-      if(err) return next(createError(403, "You are not authorized!"));
+       return next(createError(403, "You are not authorized!"));
     }
   })
 }
