27246bb892d62f40d4fb697d7e8879dacb88f442
MarindanyTracy <marindanytracy0@gmail.com>
2023-06-05 09:45:04 +0300

    Using JWT with Cookie in Node.js

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import authRoute from './routes/auth.js';
 import roomsRoute from './routes/rooms.js';
 import hotelsRoute from './routes/hotels.js';
 import usersRoute from './routes/users.js';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 dotenv.config();
@@ -22,7 +23,7 @@ mongoose.connection.on("disconnected", () => {
   console.log("mongoDB disconnected")
 })
 //middlewares
-
+app.use(cookieParser())
 app.use(express.json())
 
 app.use('/api/auth', authRoute);
