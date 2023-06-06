e30d94eb434dab383989eb778adcadfba4613644
MarindanyTracy <marindanytracy0@gmail.com>
2023-06-06 10:11:41 +0300

    Node mongoDB how to get count of documents

--- server/routes/hotels.js
+++ server/routes/hotels.js
@@ -1,18 +1,23 @@
 import express from "express";
-import { createHotel, deleteHotel, getHotel, getHotels, updateHotel } from "../controllers/hotel.js";
+import { countByCity, createHotel, deleteHotel, getHotel, getHotels, updateHotel } from "../controllers/hotel.js";
 import { verifyAdmin } from "../utils/verifyToken.js";
 
 const router = express.Router();
 
-//CREATE
-router.post('/',verifyAdmin, createHotel)
-//UPDATE
-router.put('/:id',verifyAdmin, updateHotel)
-//DELETE
-router.delete('/:id',verifyAdmin, deleteHotel)
-//GET
-router.get('/:id', getHotel)
-//GET ALL
+// CREATE
+router.post('/', verifyAdmin, createHotel)
+// UPDATE
+router.put('/:id', verifyAdmin, updateHotel)
+// DELETE
+router.delete('/:id', verifyAdmin, deleteHotel)
+// COUNT BY CITY
+router.get('/countByCity', countByCity)
+// COUNT BY TYPE
+router.get('/countByType', getHotels)
+// GET ALL
 router.get('/', getHotels)
+// GET
+router.get('/:id', getHotel)
+
 
 export default router;
