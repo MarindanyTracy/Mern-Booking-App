7ae41cb2d2e449cd9828161ae91d74f3f25664d7
MarindanyTracy <marindanytracy0@gmail.com>
2023-06-09 11:34:37 +0300

    Using reach Context to calculate price according to nights

--- client/src/pages/list/List.jsx
+++ client/src/pages/list/List.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./list.css";
-import Navbar from "../../components/navbar/Navbar";
-import Header from "../../components/header/Header";
+import Navbar from "../../components/Navbar/Navbar";
+import Header from "../../components/Header/Header";
 import { useLocation } from "react-router-dom";
 import { useState } from "react";
 import { format } from "date-fns";
